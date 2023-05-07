7d863065a43032cb9e22f265fb8f2c004f588321
Pal1 <paalinosss@gmail.com>
2023-05-07 21:24:07 +0200

    pridane subory

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Auto_ovladam_klavesnicou
+{
+    partial class Form_AutoKlaves
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_AutoKlaves));
+            this.panel_Auto = new System.Windows.Forms.Panel();
+            this.pictureBox_auto1 = new System.Windows.Forms.PictureBox();
+            this.label_koniec = new System.Windows.Forms.Label();
+            this.label_restart = new System.Windows.Forms.Label();
+            this.label_prehra = new System.Windows.Forms.Label();
+            this.pictureBox_kuzel1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_kuzel2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ciara1 = new System.Windows.Forms.PictureBox();
+            this.casovac = new System.Windows.Forms.Timer(this.components);
+            this.panel_Auto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_auto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kuzel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kuzel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_Auto
+            // 
+            this.panel_Auto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel_Auto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Auto.Controls.Add(this.pictureBox_auto1);
+            this.panel_Auto.Controls.Add(this.label_koniec);
+            this.panel_Auto.Controls.Add(this.label_restart);
+            this.panel_Auto.Controls.Add(this.label_prehra);
+            this.panel_Auto.Controls.Add(this.pictureBox_kuzel1);
+            this.panel_Auto.Controls.Add(this.pictureBox_kuzel2);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara7);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara6);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara5);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara4);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara3);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara2);
+            this.panel_Auto.Controls.Add(this.pictureBox_ciara1);
+            this.panel_Auto.Location = new System.Drawing.Point(17, 16);
+            this.panel_Auto.Margin = new System.Windows.Forms.Padding(4);
+            this.panel_Auto.Name = "panel_Auto";
+            this.panel_Auto.Size = new System.Drawing.Size(713, 339);
+            this.panel_Auto.TabIndex = 0;
+            // 
+            // pictureBox_auto1
+            // 
+            this.pictureBox_auto1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_auto1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_auto1.Image")));
+            this.pictureBox_auto1.Location = new System.Drawing.Point(54, 188);
+            this.pictureBox_auto1.Name = "pictureBox_auto1";
+            this.pictureBox_auto1.Size = new System.Drawing.Size(150, 50);
+            this.pictureBox_auto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_auto1.TabIndex = 25;
+            this.pictureBox_auto1.TabStop = false;
+            // 
+            // label_koniec
+            // 
+            this.label_koniec.AutoSize = true;
+            this.label_koniec.BackColor = System.Drawing.Color.Transparent;
+            this.label_koniec.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_koniec.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label_koniec.Location = new System.Drawing.Point(299, 311);
+            this.label_koniec.Name = "label_koniec";
+            this.label_koniec.Size = new System.Drawing.Size(91, 26);
+            this.label_koniec.TabIndex = 1;
+            this.label_koniec.Text = "KONIEC";
+            this.label_koniec.Click += new System.EventHandler(this.label_koniec_Click);
+            this.label_koniec.MouseLeave += new System.EventHandler(this.label_koniec_MouseLeave);
+            this.label_koniec.MouseHover += new System.EventHandler(this.label_koniec_MouseHover);
+            // 
+            // label_restart
+            // 
+            this.label_restart.AutoSize = true;
+            this.label_restart.BackColor = System.Drawing.Color.Transparent;
+            this.label_restart.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_restart.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label_restart.Location = new System.Drawing.Point(299, 0);
+            this.label_restart.Name = "label_restart";
+            this.label_restart.Size = new System.Drawing.Size(102, 26);
+            this.label_restart.TabIndex = 0;
+            this.label_restart.Text = "REŠTART";
+            this.label_restart.Click += new System.EventHandler(this.label_restart_Click);
+            this.label_restart.MouseLeave += new System.EventHandler(this.label_restart_MouseLeave);
+            this.label_restart.MouseHover += new System.EventHandler(this.label_restart_MouseHover);
+            // 
+            // label_prehra
+            // 
+            this.label_prehra.AutoSize = true;
+            this.label_prehra.BackColor = System.Drawing.Color.Transparent;
+            this.label_prehra.Font = new System.Drawing.Font("Berlin Sans FB", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_prehra.ForeColor = System.Drawing.Color.Red;
+            this.label_prehra.Location = new System.Drawing.Point(272, 137);
+            this.label_prehra.Name = "label_prehra";
+            this.label_prehra.Size = new System.Drawing.Size(168, 41);
+            this.label_prehra.TabIndex = 0;
+            this.label_prehra.Text = "Prehral si!";
+            this.label_prehra.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox_kuzel1
+            // 
+            this.pictureBox_kuzel1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_kuzel1.Image = global::Auto_ovladam_klavesnicou.Properties.Resources.kuzel;
+            this.pictureBox_kuzel1.Location = new System.Drawing.Point(633, 24);
+            this.pictureBox_kuzel1.Name = "pictureBox_kuzel1";
+            this.pictureBox_kuzel1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_kuzel1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_kuzel1.TabIndex = 26;
+            this.pictureBox_kuzel1.TabStop = false;
+            // 
+            // pictureBox_kuzel2
+            // 
+            this.pictureBox_kuzel2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_kuzel2.Image = global::Auto_ovladam_klavesnicou.Properties.Resources.kuzel;
+            this.pictureBox_kuzel2.Location = new System.Drawing.Point(633, 245);
+            this.pictureBox_kuzel2.Name = "pictureBox_kuzel2";
+            this.pictureBox_kuzel2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox_kuzel2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_kuzel2.TabIndex = 27;
+            this.pictureBox_kuzel2.TabStop = false;
+            // 
+            // pictureBox_ciara7
+            // 
+            this.pictureBox_ciara7.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara7.Location = new System.Drawing.Point(653, 157);
+            this.pictureBox_ciara7.Name = "pictureBox_ciara7";
+            this.pictureBox_ciara7.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara7.TabIndex = 24;
+            this.pictureBox_ciara7.TabStop = false;
+            // 
+            // pictureBox_ciara6
+            // 
+            this.pictureBox_ciara6.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara6.Location = new System.Drawing.Point(549, 157);
+            this.pictureBox_ciara6.Name = "pictureBox_ciara6";
+            this.pictureBox_ciara6.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara6.TabIndex = 23;
+            this.pictureBox_ciara6.TabStop = false;
+            // 
+            // pictureBox_ciara5
+            // 
+            this.pictureBox_ciara5.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara5.Location = new System.Drawing.Point(446, 157);
+            this.pictureBox_ciara5.Name = "pictureBox_ciara5";
+            this.pictureBox_ciara5.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara5.TabIndex = 22;
+            this.pictureBox_ciara5.TabStop = false;
+            // 
+            // pictureBox_ciara4
+            // 
+            this.pictureBox_ciara4.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara4.Location = new System.Drawing.Point(340, 157);
+            this.pictureBox_ciara4.Name = "pictureBox_ciara4";
+            this.pictureBox_ciara4.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara4.TabIndex = 21;
+            this.pictureBox_ciara4.TabStop = false;
+            // 
+            // pictureBox_ciara3
+            // 
+            this.pictureBox_ciara3.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara3.Location = new System.Drawing.Point(236, 157);
+            this.pictureBox_ciara3.Name = "pictureBox_ciara3";
+            this.pictureBox_ciara3.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara3.TabIndex = 20;
+            this.pictureBox_ciara3.TabStop = false;
+            // 
+            // pictureBox_ciara2
+            // 
+            this.pictureBox_ciara2.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara2.Location = new System.Drawing.Point(129, 157);
+            this.pictureBox_ciara2.Name = "pictureBox_ciara2";
+            this.pictureBox_ciara2.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara2.TabIndex = 19;
+            this.pictureBox_ciara2.TabStop = false;
+            // 
+            // pictureBox_ciara1
+            // 
+            this.pictureBox_ciara1.BackColor = System.Drawing.Color.White;
+            this.pictureBox_ciara1.Location = new System.Drawing.Point(25, 157);
+            this.pictureBox_ciara1.Name = "pictureBox_ciara1";
+            this.pictureBox_ciara1.Size = new System.Drawing.Size(30, 8);
+            this.pictureBox_ciara1.TabIndex = 18;
+            this.pictureBox_ciara1.TabStop = false;
+            // 
+            // casovac
+            // 
+            this.casovac.Enabled = true;
+            this.casovac.Interval = 15;
+            this.casovac.Tick += new System.EventHandler(this.casovac_Tick);
+            // 
+            // Form_AutoKlaves
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(747, 370);
+            this.Controls.Add(this.panel_Auto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form_AutoKlaves";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vyhnite sa prekážkam";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_AutoKlaves_KeyDown);
+            this.panel_Auto.ResumeLayout(false);
+            this.panel_Auto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_auto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kuzel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_kuzel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ciara1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Auto;
+        private System.Windows.Forms.PictureBox pictureBox_ciara7;
+        private System.Windows.Forms.PictureBox pictureBox_ciara6;
+        private System.Windows.Forms.PictureBox pictureBox_ciara5;
+        private System.Windows.Forms.PictureBox pictureBox_ciara4;
+        private System.Windows.Forms.PictureBox pictureBox_ciara3;
+        private System.Windows.Forms.PictureBox pictureBox_ciara2;
+        private System.Windows.Forms.PictureBox pictureBox_ciara1;
+        private System.Windows.Forms.Timer casovac;
+        private System.Windows.Forms.PictureBox pictureBox_auto1;
+        private System.Windows.Forms.PictureBox pictureBox_kuzel1;
+        private System.Windows.Forms.PictureBox pictureBox_kuzel2;
+        private System.Windows.Forms.Label label_prehra;
+        private System.Windows.Forms.Label label_restart;
+        private System.Windows.Forms.Label label_koniec;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_ovladam_klavesnicou
+{
+    public partial class Form_AutoKlaves : Form
+    {
+        public Form_AutoKlaves()
+        {
+            InitializeComponent(); 
+            label_prehra.Hide();
+            label_restart.Hide();
+            label_koniec.Hide();
+        }
+
+        private void Prehra()
+        {
+            if(pictureBox_auto1.Bounds.IntersectsWith(pictureBox_kuzel1.Bounds))
+            {
+                casovac.Stop();
+                label_prehra.Show();
+                label_restart.Show();
+                label_koniec.Show();
+            }
+            if (pictureBox_auto1.Bounds.IntersectsWith(pictureBox_kuzel2.Bounds))
+            {
+                casovac.Stop();
+                label_prehra.Show();
+                label_restart.Show();
+                label_koniec.Show();
+            }
+        }
+
+        public void ciaryPohyb()
+        {
+            if (pictureBox_ciara1.Left <= 0)
+            {
+                pictureBox_ciara1.Left = 550;
+            }
+            else if (pictureBox_ciara2.Left <= 0)
+            {
+                pictureBox_ciara2.Left = 550;
+            }
+            else if (pictureBox_ciara3.Left <= 0)
+            {
+                pictureBox_ciara3.Left = 550;
+            }
+            else if (pictureBox_ciara4.Left <= 0)
+            {
+                pictureBox_ciara4.Left = 550;
+            }
+            else if (pictureBox_ciara5.Left <= 0)
+            {
+                pictureBox_ciara5.Left = 550;
+            }
+            else if (pictureBox_ciara6.Left <= 0)
+            {
+                pictureBox_ciara6.Left = 550;
+            }
+            else if (pictureBox_ciara7.Left <= 0)
+            {
+                pictureBox_ciara7.Left = 550;
+            }
+
+            else
+            {
+                pictureBox_ciara1.Left -= 5;
+                pictureBox_ciara2.Left -= 5;
+                pictureBox_ciara3.Left -= 5;
+                pictureBox_ciara4.Left -= 5;
+                pictureBox_ciara5.Left -= 5;
+                pictureBox_ciara6.Left -= 5;
+                pictureBox_ciara7.Left -= 5;
+            }
+            Random nahoda = new Random();
+            int x, y;
+            if(pictureBox_kuzel1.Left<=0)
+            {
+                x = nahoda.Next(0, 220);
+                pictureBox_kuzel1.Location = new Point(480, x);
+            }
+            else if (pictureBox_kuzel2.Left <= 0)
+            {
+                y = nahoda.Next(0, 220);
+                pictureBox_kuzel2.Location = new Point(480, y);
+            }
+            else
+            {
+                pictureBox_kuzel1.Left -= 3;
+                pictureBox_kuzel2.Left -= 2;
+            }    
+        }
+
+        private void Form_AutoKlaves_KeyDown(object sender, KeyEventArgs e)
+        {
+            //reakcia auta na šípky
+            /*switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    x -= 5;
+                    break;
+                case Keys.Right:
+                    x += 5;
+                    break;
+                case Keys.Up:
+                    y -= 5;
+                    break;
+                case Keys.Down:
+                    y += 5;
+                    break;
+            }*/
+
+            if (e.KeyCode == Keys.Up)
+            {
+                if (pictureBox_auto1.Top > 19)
+                {
+                    pictureBox_auto1.Top -= 13;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (pictureBox_auto1.Top < 217)
+                {
+                    pictureBox_auto1.Top += 13;
+                }
+            }
+
+            panel_Auto.Refresh();
+        }
+
+        private void casovac_Tick(object sender, EventArgs e)
+        {
+            ciaryPohyb();
+            Prehra();
+        }
+
+        private void label_restart_Click(object sender, EventArgs e)
+        {
+            casovac.Start();
+            pictureBox_kuzel1.Location = new Point(480, 25);
+            pictureBox_kuzel2.Location = new Point(480, 200);
+            label_prehra.Hide();
+            label_koniec.Hide();
+        }
+
+        private void label_koniec_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label_restart_MouseHover(object sender, EventArgs e)
+        {
+            label_restart.BackColor = Color.White;
+        }
+
+        private void label_restart_MouseLeave(object sender, EventArgs e)
+        {
+            label_restart.BackColor = Color.Transparent;
+        }
+
+        private void label_koniec_MouseHover(object sender, EventArgs e)
+        {
+            label_koniec.BackColor = Color.White;
+        }
+
+        private void label_koniec_MouseLeave(object sender, EventArgs e)
+        {
+            label_koniec.BackColor= Color.Transparent;
+        }
+    }
+}
